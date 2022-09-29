e5b4e791b169283407d3340e4081ce76e60575dd
tito23 <jorge591vl@gmail.com>
2022-09-29 17:39:34 -0500

    cerrar sesion hecho

--- Pagina-Eventos/app.js
+++ Pagina-Eventos/app.js
@@ -422,6 +422,38 @@ function off(){
 
 // boton de jorge
 
-botonCerrar.addEventListener('click',()=>{
-    console.log("holi");
-})
+
+       // Import the functions you need from the SDKs you need
+       import { initializeApp } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-app.js";
+       import { getAuth, signOut} from "https://www.gstatic.com/firebasejs/9.10.0/firebase-auth.js";
+       import { getDatabase, set, ref , update} from "https://www.gstatic.com/firebasejs/9.10.0/firebase-database.js";
+       // TODO: Add SDKs for Firebase products that you want to use
+       // https://firebase.google.com/docs/web/setup#available-libraries
+       const firebaseConfig = {
+         apiKey: "AIzaSyC5b9YZ525VgxChtH0WBgdmuI9jpF4t8v0",
+         authDomain: "login-eventos-5e0c0.firebaseapp.com",
+         databaseURL: "https://login-eventos-5e0c0-default-rtdb.firebaseio.com",
+         projectId: "login-eventos-5e0c0",
+         storageBucket: "login-eventos-5e0c0.appspot.com",
+         messagingSenderId: "713108752407",
+         appId: "1:713108752407:web:27d3a2a04dcb183fb73452"
+       };
+     
+       // Initialize Firebase
+       const app = initializeApp(firebaseConfig);
+       const auth = getAuth();
+        const database = getDatabase(app);
+
+        botonCerrar.addEventListener('click',()=>{
+            Swal.fire({
+                position: 'center',
+                icon: 'success',
+                title: 'Cerraste sesion correctamente',
+                showConfirmButton: false,
+                timer: 1500,
+                timerProgressBar: true
+              })
+              .then(()=>{
+                window.location.href = "indexPrincipal.html";
+              })
+    })
