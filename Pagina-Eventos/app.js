086c1804fe07b55fbedcab299302c3f2b5417d37
workservices123 <workservices283@gmail.com>
2022-09-29 17:09:33 -0500

    cambio en app

--- Pagina-Eventos/app.js
+++ Pagina-Eventos/app.js
@@ -409,8 +409,7 @@ function off(){
 
     main.style.background = '#c5d7ff'
     main2.style.background = '#c5d7ff'
-    main.style.transition ='all 1000ms ease'
-    main2.style.transition ='all 1000ms ease'
+    
 
     menu.style.background = '#407aff'
     menu.style.transition = 'all 1000ms ease'
